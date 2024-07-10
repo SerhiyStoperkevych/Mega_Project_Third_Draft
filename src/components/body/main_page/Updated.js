bff9d11fe7780fc694e128cf3326f0558cc876e2
SerhiyStoperkevych <116464169+SerhiyStoperkevych@users.noreply.github.com>
2024-07-10 17:24:28 -0400

    Create Updated.js

---
+++ src/components/body/main_page/Updated.js
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react';
+import {AppContext} from '../../../AppContext'; 
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Link } from 'react-router-dom';
+
+function Updated() {
+
+    const { data } = useContext(AppContext);
+
+    return(
+            <div className="updated">
+                <h2>Recent</h2>
+                <ul>
+                    {data.map(item => (
+                        <li key={item.id}>
+                            <Link to="/main" className="link">
+                                <img src={item.picture} alt={item.name} />
+                                <div>
+                                    <h3>{item.name}</h3>
+                                    <p>Chapter: {item.chapter}</p>
+                                    <p><i className="bi bi-alarm"></i> {item.time} minutes ago</p>
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+    )
+}
+
+export default Updated;
