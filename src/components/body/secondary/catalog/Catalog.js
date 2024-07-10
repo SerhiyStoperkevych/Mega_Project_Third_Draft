adbe2e517572f6a9b9ea300cd911a918c1dc3503
SerhiyStoperkevych <116464169+SerhiyStoperkevych@users.noreply.github.com>
2024-07-10 17:17:10 -0400

    Create Catalog.js

---
+++ src/components/body/secondary/catalog/Catalog.js
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react';
+import {AppContext} from '../../../../AppContext'; 
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Link } from 'react-router-dom';
+
+const Catalog = () => {
+    const { data } = useContext(AppContext);
+
+    return(
+            <div className='catalog'>
+                <h2>Catalog</h2>
+                <h3>Genres</h3>
+                <ul>
+                    {data.map(item => (
+                        <li key={item.id}>
+                            <Link to="/main" className="link">
+                                <img src={item.picture} alt={item.name} />
+                                <div>
+                                    <h3>{item.name}</h3>
+                                    <p>Chapter: {item.chapter}</p>
+                                    <p><i className="bi bi-alarm"></i> {item.time} minutes ago</p>
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+    );
+}
+
+export default Catalog;
