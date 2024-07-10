331dbe0d335774149826665ea3a21a82753ef8c9
SerhiyStoperkevych <116464169+SerhiyStoperkevych@users.noreply.github.com>
2024-07-10 17:23:49 -0400

    Create Policy.js

---
+++ src/components/body/main_page/Policy.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function Policy() {
+
+    return(
+            <div className="policy">
+
+                    <div className='text'>
+
+                        <h3> Policy #1 </h3>
+
+                        <h3> Policy #2 </h3>
+
+                        <h3> Policy #3 </h3>
+
+                        <h3> Policy #4 </h3>
+
+                    </div>
+
+                    <hr />
+
+                    <div className='description'>
+
+                        <h3> Why do you need this cite? </h3>
+
+                        <p> La final de la Copa Mundial de Fútbol de 2010 fue el partido final de la Copa del Mundo de dicho año, la 19.ª edición de la competición de la FIFA para selecciones nacionales de fútbol. El partido se jugó en el Soccer City de Johannesburgo, Sudáfrica, el 11 de julio de 2010, y lo disputaron los equipos de Países Bajos y España. En el certamen participaron el país anfitrión, Sudáfrica, y otras treinta y una selecciones surgidas de la fase de clasificación, organizada por las seis confederaciones de la FIFA.  </p>
+
+                        <p> We need you as one of our most profound user. </p>
+
+                        <h3> Hello </h3>
+
+                    </div>
+
+            </div>
+    )
+}
+
+export default Policy;
